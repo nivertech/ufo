cff0de7ab20bc2695b3859366e3d1aa60f63f171
Jonathan Foster <jxf9099@rit.edu>
2014-05-17 20:00:47 -0400

    location page works yay

--- app/scripts/controllers/location.js
+++ app/scripts/controllers/location.js
@@ -1,30 +1,30 @@
 'use strict';
 
 app.controller('LocationCtrl', function ($scope, LocationService, GeocodeService, NUFORCService) {
-  $scope.position = null;
+  $scope.latitude = null;
+  $scope.longitude = null;
 
   LocationService().then(function(position){
-    $scope.position = position.coords;
+    $scope.latitude = position.coords.latitude;
+    $scope.longitude = position.coords.longitude;
 
     GeocodeService.getAddress(position.coords)
       .success(function(data){
-        console.log(data);
-        // var location = data.results[0].address_components;
+        var location = data.location;
 
-        // $scope.state = location[5].long_name;
-        // $scope.abbr = location[5].short_name;
-        // $scope.city = location[2].long_name;
+        $scope.state = location.state;
+        $scope.city = location.city;
 
-        // var params = ['location', $scope.abbr, $scope.city];
+        var params = ['location', $scope.state, $scope.city];
 
-        // NUFORCService.getSightings(params)
-        //   .success(function(data){
-        //     $scope.sightings = data;
-        //   })
-        //   .error(function(data, status){
-        //     $scope.sightings = data || 'Error retrieving reported UFO sightings.';
-        //     console.log(status);
-        //   });
+        NUFORCService.getSightings(params)
+          .success(function(data){
+            $scope.sightings = data;
+          })
+          .error(function(data, status){
+            $scope.sightings = data || 'Error retrieving reported UFO sightings.';
+            console.log(status);
+          });
       })
       .error(function(data, status){
         $scope.location = data || 'Could not retrieve location information';
@@ -35,38 +35,31 @@ app.controller('LocationCtrl', function ($scope, LocationService, GeocodeService
   document.getElementById('zip').addEventListener('keyup', function(e){
     if (e.keyCode == 13){
 
+      console.log('looking up location');
+
       GeocodeService.setLocation($scope.zip)
         .success(function(data){
           console.log(data);
-          // $scope.position.latitude = data.results[0].geometry.location.lat;
-          // $scope.position.longitude = data.results[0].geometry.location.lng;
-        })
-          .then(GeocodeService.getAddress($scope.position)
+          $scope.latitude = data.location.lat;
+          $scope.longitude = data.location.lon;
+          $scope.state = data.location.state;
+          $scope.city = data.location.city;
+
+          var params = ['location', $scope.state, $scope.city];
+
+          NUFORCService.getSightings(params)
             .success(function(data){
-              console.log(data);
-              /*
-              var location = data.results[0].address_components;
-
-              $scope.state = location[5].long_name;
-              $scope.abbr = location[5].short_name;
-              $scope.city = location[2].long_name;
-
-              var params = ['location', $scope.abbr, $scope.city];
-
-              NUFORCService.getSightings(params)
-                .success(function(data){
-                  $scope.sightings = data;
-                })
-                .error(function(data, status){
-                  $scope.sightings = data || 'Error retrieving reported UFO sightings.';
-                  console.log(status);
-                });*/
+              $scope.sightings = data;
             })
             .error(function(data, status){
-              $scope.location = data || 'Could not retrieve location information';
+              $scope.sightings = data || 'Error retrieving reported UFO sightings.';
               console.log(status);
-            })
-          );
+            });
+        })
+        .error(function(data, status){
+          $scope.location = data || 'Could not retrieve location information';
+          console.log(status);
+        });
     }
   });
 
