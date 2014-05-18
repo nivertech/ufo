bb149211df05aad860899511bd50ae521576c3ef
Jonathan Foster <jxf9099@rit.edu>
2014-05-18 00:00:36 -0400

    allow user to enter specific date on history page

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -1,14 +1,19 @@
 'use strict';
 
 app.controller('HistoryCtrl', function ($scope, NUFORCService) {
-  var params = ['date', '2013'];
 
-  NUFORCService.getSightings(params)
-    .success(function(data){
-      $scope.sightings = data;
-    })
-    .error(function(data, status){
-      $scope.sightings = data || 'Error retrieving reported UFO sightings.';
-      $scope.status = status;
-    });
+  $scope.setDate = function(){
+    console.log('yo');
+    var params = ['date', $scope.date.year, $scope.date.month, $scope.date.day];
+
+    NUFORCService.getSightings(params)
+      .success(function(data){
+        $scope.sightings = data;
+      })
+      .error(function(data, status){
+        $scope.sightings = data || 'Error retrieving reported UFO sightings.';
+        $scope.status = status;
+      });
+  };
+
 });
